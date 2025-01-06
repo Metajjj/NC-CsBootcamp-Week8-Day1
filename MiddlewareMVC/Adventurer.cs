e0572d81ac0be569a551970ff2811de052d44cb2
Metajjj <joolsjannaway@gmail.com>
2025-01-06 10:29:16 +0000

    Task1 - adding adventurer class

---
+++ MiddlewareMVC/Adventurer.cs
@@ -0,0 +1,23 @@
+﻿namespace MiddlewareMVC
+{
+
+    public enum FightingClasses
+    {
+        Warrior, Rogue, Mage
+    }
+
+    public class Adventurer
+    {
+        public int Id { get; set; } = ++IdCnt;
+        public static int IdCnt = 0;
+        public string Name { get; set; }
+        public FightingClasses FightingClass { get; set; };
+        public int Level { get; set; } = 1;
+        public int XP { get; set; } = 0;
+
+        public Adventurer(string name, FightingClasses Fc)
+        {
+            Name = name; FightingClass= Fc;
+        }
+    }
+}
